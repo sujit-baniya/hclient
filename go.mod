6fe6266bb4ccded03b55feb127b122d417793c4c
sujit <itsursujit@gmail.com>
2020-12-10 23:21:02 +0545

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sujit-baniya/hclient
+
+go 1.15
+
+require (
+	github.com/hetiansu5/urlquery v1.2.3
+	github.com/m-murad/ordered-sync-map v0.0.0-20200808201854-ff3313305f2d
+)
---
+++ methods.go
@@ -0,0 +1,67 @@
+package hclient
+
+import (
+	"github.com/hetiansu5/urlquery"
+	mp "github.com/m-murad/ordered-sync-map"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+var mu sync.RWMutex
+
+// Get is a convenience helper for doing simple GET requests.
+func (c *Client) Get(url string, body interface{}, headers *mp.Map) (*http.Response, error) {
+	bytes, _ := urlquery.Marshal(body)
+	url = url + "?" + string(bytes)
+	return c.request(http.MethodGet, url, nil, headers)
+}
+
+// Get is a convenience helper for doing simple GET requests.
+func (c *Client) GetJson(url string, body interface{}, headers *mp.Map) (*http.Response, error) {
+	headers.Put("Content-Type", "application/json")
+	return c.Get(url, body, headers)
+}
+
+// Post is a convenience method for doing simple POST requests.
+func (c *Client) Post(url string, body interface{}, headers *mp.Map) (*http.Response, error) {
+	return c.request(http.MethodPost, url, body, headers)
+}
+
+// Post is a convenience method for doing simple POST requests.
+func (c *Client) PostJson(url string, body interface{}, headers *mp.Map) (*http.Response, error) {
+	headers.Put("Content-Type", "application/json")
+	return c.Post(url, body, headers)
+}
+
+// PostForm is a convenience method for doing simple POST operations using
+// pre-filled url.Values form data.
+func (c *Client) PostForm(url string, data url.Values, headers *mp.Map) (*http.Response, error) {
+	headers.Put("Content-Type", "application/x-www-form-urlencoded")
+	return c.Post(url, strings.NewReader(data.Encode()), headers)
+}
+
+// Get is a convenience helper for doing simple GET requests.
+func (c *Client) Delete(url string, headers *mp.Map) (*http.Response, error) {
+	return c.request(http.MethodDelete, url, nil, headers)
+}
+
+// Head is a convenience method for doing simple HEAD requests.
+func (c *Client) Head(url string, headers *mp.Map) (*http.Response, error) {
+	return c.request(http.MethodHead, url, nil, headers)
+}
+
+func (c *Client) request(method string, url string, body interface{}, headers *mp.Map) (*http.Response, error) {
+	req, err := NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	headers.UnorderedRange(func(key interface{}, value interface{}) {
+		req.Header.Set(key.(string), value.(string))
+	})
+	/*for key, value := range headers {
+		req.Header.Set(key, value)
+	}*/
+	return c.Do(req)
+}
